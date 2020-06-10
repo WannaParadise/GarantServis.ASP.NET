ef43609b7652d9781f6c95fcd8edff9fe60a351e
WannaParadise <fsc.prusevic@bsu.by>
2020-06-10 10:34:43 +0300

    Update ChatHub.cs

--- Garant/Models/ChatHub/ChatHub.cs
+++ Garant/Models/ChatHub/ChatHub.cs
@@ -8,6 +8,7 @@ namespace Garant.Models.ChatHub
 {
     public class ChatHub : Hub
     {
+        //
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
